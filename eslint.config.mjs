6252d7f3c3ba7f88e2b8b4c0ceaa625bc9dac39e
qcharlieshi <charlies@live.com>
2026-05-26 12:27:15 -0400

    chore: add eslint/prettier config and untrack IDE/OS files

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+
+export default [
+  {
+    ignores: ["**/dist/**", "**/node_modules/**", "legacy/**", "**/*.tsbuildinfo"],
+  },
+  js.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
+  },
+];
